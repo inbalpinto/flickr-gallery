6f366c4f779ff09bedaaf33dd3a64e2ca030dc3a
inbalpinto <inbal.pinto@gmail.com>
2019-01-31 10:37:32 +0200

    Updates

---
+++ precache-manifest.ffc059182602fd5b22b74e9ffef7362a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "876cc104004a896abfc2aadd3d4b77cc",
+    "url": "/flickr-gallery/static/media/magnifying-glass.876cc104.svg"
+  },
+  {
+    "revision": "f93d0c1858df00a3661b",
+    "url": "/flickr-gallery/static/js/runtime~main.f93d0c18.js"
+  },
+  {
+    "revision": "ad12280207dfb8e7899f",
+    "url": "/flickr-gallery/static/js/main.ad122802.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/flickr-gallery/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ad12280207dfb8e7899f",
+    "url": "/flickr-gallery/static/css/main.9ed9c583.chunk.css"
+  },
+  {
+    "revision": "ab7addde41f056892a3f3613bb0cae35",
+    "url": "/flickr-gallery/index.html"
+  }
+];
